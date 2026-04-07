b6a4bd6e79c2a4b9acad31059b647660cba2a1ec
guo20082200 <20364270+guo20082200@users.noreply.github.com>
2026-04-07 07:22:20 -0400

    Initialize project as Spring Boot with Maven and JDK 17

---
+++ src/main/java/com/ontologyos/ontologyos/OntologyOsApplication.java
@@ -0,0 +1,13 @@
+package com.ontologyos.ontologyos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OntologyOsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OntologyOsApplication.class, args);
+    }
+
+}
